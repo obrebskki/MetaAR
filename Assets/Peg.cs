e911901817e9510695000043210bb15c67a52699
obrebskki <obrebskirafal@gmail.com>
2018-08-23 10:13:08 +0200

    Added new images to recognize by Vuforia

--- Assets/Peg.cs
+++ Assets/Peg.cs
@@ -1,42 +1,42 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿//using UnityEditor;
+//using UnityEngine;
 
-public class Peg : PegBase {
+//public class Peg : PegBase {
 
-    protected override void Awake()
-    {
-        base.Awake();
-        GetComponent<HandClick>().OnClicked += _ => RequestRotation();
-    }
+//    protected override void Awake()
+//    {
+//        base.Awake();
+//        GetComponent<HandClick>().OnClicked += _ => RequestRotation();
+//    }
 
-    public override void RequestRotation()
-    {
-        TargetAngle += 90;
-        FindObjectOfType<MainPeg>().RequestRotation();
-    }
+//    public override void RequestRotation()
+//    {
+//        TargetAngle += 90;
+//        FindObjectOfType<MainPeg>().RequestRotation();
+//    }
 
-    public override void RotateImmediate()
-    {
-        currentRotation.y -= 90;
-        TargetAngle -= 90;
-        transform.localRotation = Quaternion.Euler(currentRotation);
-        FindObjectOfType<MainPeg>().RotateImmediate();
-    }
-}
+//    public override void RotateImmediate()
+//    {
+//        currentRotation.y -= 90;
+//        TargetAngle -= 90;
+//        transform.localRotation = Quaternion.Euler(currentRotation);
+//        FindObjectOfType<MainPeg>().RotateImmediate();
+//    }
+//}
 
-[CustomEditor(typeof(Peg))]
-[DisallowMultipleComponent]
-[CanEditMultipleObjects]
-public class PegEditor : Editor {
+//[CustomEditor(typeof(Peg))]
+//[DisallowMultipleComponent]
+//[CanEditMultipleObjects]
+//public class PegEditor : Editor {
 
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
+//    public override void OnInspectorGUI()
+//    {
+//        DrawDefaultInspector();
 
-        if (GUILayout.Button("Rotate"))
-        {
-            (target as Peg).RequestRotation();
-        }
-    }
+//        if (GUILayout.Button("Rotate"))
+//        {
+//            (target as Peg).RequestRotation();
+//        }
+//    }
 
-}
+//}
